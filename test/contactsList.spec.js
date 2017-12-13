47d9075bc7358359ede449e6e642d0cc81b15d74
nattapong <58160388@go.buu.ac.th>
2017-12-13 17:34:08 +0700

    GET for id

--- test/contactsList.spec.js
+++ test/contactsList.spec.js
@@ -20,3 +20,20 @@ describe('GET /contacts',()=>{
     })
   })
   })
+
+  describe('GET /contacts',()=>{
+    it('Get id:1 and ok status 200 if ok',(done)=>{
+      request(router).get('/contacts/1')
+      .expect(200)
+      .then((res)=>{
+        let contact = res.body
+        expect(contact.id).toBeDefined()
+        expect(contact.name).toBeDefined()
+        expect(contact.email).toBeDefined()
+        expect(contact.phone).toBeDefined()
+        expect(contact.url).toBeDefined()
+        expect(contact.notes).toBeDefined()
+        done()
+      })
+    })
+    })
